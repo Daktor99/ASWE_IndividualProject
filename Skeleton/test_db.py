b5b07b46190e3ce086358d1f8a696bacf6bddea8
Daktor99 <ed2918@columbia.edu>
2021-10-07 20:53:20 -0400

    Adding skeleton for unit testing of db.py

--- Skeleton/test_db.py
+++ Skeleton/test_db.py
@@ -1,8 +1,13 @@
 import unittest
+from unittest.mock import patch
+import db
 
-class MyTestCase(unittest.TestCase):
+class Test_Testdb(unittest.TestCase):
+
+
     def test_something(self):
         self.assertEqual(True, False)
+
 
 if __name__ == '__main__':
     unittest.main()
